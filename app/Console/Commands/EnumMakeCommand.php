281a19df1054ebd02f0ec3338cd33f73d77807a3
Utsav <utsavsomaiya4464@gmail.com>
2023-07-04 12:55:54 +0530

    stub publish

---
+++ app/Console/Commands/EnumMakeCommand.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+
+#[AsCommand(name: 'make:enum')]
+class EnumMakeCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:enum {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new enum';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Enum';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return $this->resolveStubPath('/stubs/enum.stub');
+    }
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+    protected function resolveStubPath($stub)
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : __DIR__.$stub;
+    }
+
+    /**
+     * Replace the class name for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $name
+     * @return string
+     */
+    protected function replaceClass($stub, $name)
+    {
+        $stub = parent::replaceClass($stub, $name);
+
+        return str_replace('{{ enum }}', Str::of($name)->classBasename()->title()->value(), $stub);
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        $name = Str::replaceFirst($this->rootNamespace(), '', $name);
+
+        return app_path().str_replace('\\', '/Enums/', $name).'.php';
+    }
+
+    /**
+     * Get the root namespace for the class.
+     *
+     * @return string
+     */
+    protected function rootNamespace()
+    {
+        return 'App\Enums';
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array<int, array<int, int|string>>
+     */
+    protected function getArguments(): array
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the enum'],
+        ];
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -16,6 +16,32 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
+Artisan::command('dd {code*}', function () {
+    $isAllowedToRun = function () {
+        if (env('ALLOW_DD_COMMAND') === true) {
+            return true;
+        }
+
+        return app()->environment('local');
+    };
+
+    if (! $isAllowedToRun()) {
+        $this->error('This command can only run if the environment variable `ALLOW_DD_COMMAND` is set to `true` or in local environment');
+
+        return;
+    }
+
+    return collect($this->argument('code'))
+        ->map(function (string $command) {
+            return rtrim($command, ';');
+        })
+        ->map(function (string $sanitizedCommand) {
+            return eval("dump({$sanitizedCommand});");
+        })
+        ->implode(PHP_EOL);
+
+})->purpose('Run the given code and dump the result');
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
