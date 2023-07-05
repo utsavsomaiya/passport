76fbea400b53c6fafdab248e9f56a85f815104ab
Utsav <utsavsomaiya4464@gmail.com>
2023-07-05 17:08:06 +0530

    company migrations

---
+++ database/migrations/2023_07_05_112622_create_company_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_user', function (Blueprint $table) {
+            $table->primary(['company_id', 'user_id']);
+            $table->foreignIdFor(Company::class, 'company_id')->constrained('companies');
+            $table->foreignIdFor(User::class, 'user_id')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_user');
+    }
+};
