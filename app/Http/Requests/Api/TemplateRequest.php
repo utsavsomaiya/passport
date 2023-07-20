70a526eb9a04968d37fd37dc964e490f4f511ac8
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2023-07-20 18:10:44 +0530

    template rule

--- app/Http/Requests/Api/TemplateRequest.php
+++ app/Http/Requests/Api/TemplateRequest.php
@@ -6,6 +6,7 @@ namespace App\Http\Requests\Api;
 
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class TemplateRequest extends FormRequest
 {
@@ -16,8 +17,13 @@ class TemplateRequest extends FormRequest
      */
     public function rules(): array
     {
+        $templateId = null;
+        if ($this->route()?->getName() === 'api.templates.update') {
+            $templateId = $this->route()->parameter('id');
+        }
+
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255', Rule::unique('templates', 'name')->ignore($templateId)->where('company_id', app('company_id'))],
             'description' => ['nullable', 'string'],
             'slug' => ['sometimes', 'string', 'max:255'],
         ];
