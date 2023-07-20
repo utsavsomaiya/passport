8294a0d6e9703ab11fd58306264475339d190f66
Gaurav <gauravmakhecha@gmail.com>
2023-07-20 17:36:57 +0530

    config: max results for pagination

--- config/json-api-paginate.php
+++ config/json-api-paginate.php
@@ -8,7 +8,7 @@ return [
      * The maximum number of results that will be returned
      * when using the JSON API paginator.
      */
-    'max_results' => 25,
+    'max_results' => 100,
 
     /*
      * The default number of results that will be returned
