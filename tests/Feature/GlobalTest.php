5daccf2581baa5891b0b9b4b2ff7a27d38964e91
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2023-08-03 11:37:22 +0530

    code: refactor

--- tests/Feature/GlobalTest.php
+++ tests/Feature/GlobalTest.php
@@ -3,14 +3,10 @@
 declare(strict_types=1);
 
 test('all migrations should have not use down method', function (): void {
-    $migrationPath = database_path('migrations');
-    $migrationFiles = scandir($migrationPath);
+    $migrationFiles = array_values(array_diff(scandir(database_path('migrations')), ['..', '.']));
 
-    foreach ($migrationFiles as $file) {
-        if (strpos($file, '.php') !== false) {
-            $migrationInstance = require database_path('migrations/' . $file);
-
-            $this->assertFalse(method_exists($migrationInstance, 'down'));
-        }
+    foreach ($migrationFiles as $migrationFile) {
+        $migrationInstance = require database_path('migrations/' . $migrationFile);
+        $this->assertFalse(method_exists($migrationInstance, 'down'));
     }
 });
