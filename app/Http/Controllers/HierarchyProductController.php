1e8be0419cd670c0e886ea86ce6751ea9819f2a9
Utsav Somaiya <utsavsomaiya4464@gmail.com>
2023-08-25 18:42:42 +0530

    code: refactor

--- app/Http/Controllers/HierarchyProductController.php
+++ app/Http/Controllers/HierarchyProductController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Api\CreateOrUpdateHierarchyProductRequest;
 use App\Http\Requests\Api\FetchHierarchyProductRequest;
 use App\Http\Requests\Api\HierarchyProductRequest;
 use App\Http\Resources\Api\HierarchyProductResource;
